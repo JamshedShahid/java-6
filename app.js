78ca132b4f6cd9096fec4629abe5e417ab2141ef
Muhaamd Jamshed <eyes0033@gmail.com>
2021-11-17 00:30:22 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,102 @@
+// alert("Jamshed Shahid")
+
+
+// Q===
+
+
+// function foo(){
+//     var na= new Date()
+//     document.write(na)
+// }
+// foo()
+
+// Q========
+
+// function f (firstName,LastName){
+//     document.write(firstName+ " " + LastName+ " "+ "Assalamualikum")
+// }
+// var firstName = prompt("ENTE FirstName ")
+// var LastName = prompt("ENTE lastName ")
+// f(firstName,LastName)
+
+// Q=========
+// function fo (num1,num2){
+//     return num1 +num2
+// }
+// 
+// console.log(fo(20,30))
+
+
+// Q======
+
+// function add (num1 , num2){
+//    return num1+num2
+// }
+// add(20,30,30)
+
+// function add (num1 , num2){
+//     alert( num1+num2)
+//  }
+//  add(20,30,30)
+
+
+
+// Q===
+
+
+// function area(width,height){
+//     document.write("The width of your rectangle is"+width*height+" ")
+//     document.write("<br>"+"The height of your rectangle is"+width+height)
+    
+// }
+// var width = prompt("Enter a width of your rectangle.");
+//  var height = prompt("Enter a  width of your rectangle.");
+// area(width,height)
+
+// Q=====
+// function re(tr){
+//     var rev ="";
+//     for(var char of tr){
+//         rev = char+rev
+//     }
+//     return rev === tr
+// }
+// console.log(re('12'))
+
+// Q============
+
+// function up(str){
+//     var na= str.split(' ')
+//     var array=[]
+//     for (var x=0;x<na.length;x++){
+//         array.push(na[x].charAt(0).toUpperCase()+na[x].slice(1))
+//     }
+//      return array.join(' ')
+
+// }
+// console.log(up("the quick brown fox"))
+
+
+// // Q======
+// function find(y,z){
+//     var Count = 0;
+//     for(var p =0;p<y.length;p++ ){
+//         if(y.charAt(p)==z){
+//             Count+=1
+//         }
+//     }
+//     return Count
+// }
+// console.log(find('JSResourceS.com', 'o'))
+
+
+// Q======
+// function ad(val1,val2){
+//     var d = Math.pow(val1,val2)
+//     alert(d);
+//     return d;
+// }
+// ad("8","6")
+// console.log(ad(20,7))
+
+  
